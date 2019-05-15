573ef284e78789064a35fe7b17350ae03d4cfe0e
wojciech-kniec <kniecwojciech@gmail.com>
2019-05-15 10:09:31 +0200

    Note class added

---
+++ src/main/java/com/infoshareacademy/springdemo/Note.java
@@ -0,0 +1,45 @@
+package com.infoshareacademy.springdemo;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "content")
+    private String content;
+    @Column(name = "date")
+    private LocalDate date;
+
+    public Note(String content, LocalDate date) {
+        this.content = content;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
